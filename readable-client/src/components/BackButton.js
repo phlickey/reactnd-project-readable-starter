172306103171a717d13f4cbe181cd1112fbccfc9
phlickey <phlickey@gmail.com>
2018-06-24 15:04:04 -0400

    FEAT: Making back button component.

---
+++ readable-client/src/components/BackButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function BackButton(){
+    return (
+        <button onClick={()=>{window.history.back();}}>Back</button>
+    )
+}
+export default BackButton;
--- readable-client/src/components/Post.js
+++ readable-client/src/components/Post.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import BackButton from './BackButton';
 class Post extends Component {
     render(){
         let {category, postId} = this.props.match.params;
         let post = this.props.posts[postId];
         let {title, body, author, timestamp, commentCount, voteScore} = post;
         return (
-            <div>
+            <div className="post">
                 <Link to={`/${category}/${postId}`}> <h1>{ title }</h1> </Link>
-                <button>Back</button> 
+                <BackButton />
                 <p>{ body }</p>
                 <p>Score: {voteScore} </p>
                 <p>{ commentCount } { (commentCount > 1) ? 'comments' : 'comment'} </p>
                 <sub>Posted by: { author } at { timestamp }</sub>
                 <button>Edit</button>
                 <button>Delete</button>
+                <div>
+
+                </div>
             </div>
         )
     }
